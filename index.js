b019eaa65c8622bc3c982d6943a67b60544d2d87
Aimee Liang <aimeeeliang@gmail.com>
2020-04-17 22:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(array1, array2){
+  let whatIsPlayed = [];
+  for (let i = 0; i < array1.length; i++){
+  whatIsPlayed.push(`${array1[i]} plays ${array2[i]}`);
+  }
+  return whatIsPlayed;
+}
+
+function johnLennonFacts(array){
+  let finalFacts = [];
+  let i = 0;
+  while (i <= array.length){
+    finalFacts.push(`${array}!!!`);
+  }
+  return finalFacts;
+} 
+
+function iLoveTheBeatles(num){
+  let result = [];
+  do {
+    result.push("I love the Beatles!")
+    num++;
+  }
+  while (num < 15);
+  return result
+} 
